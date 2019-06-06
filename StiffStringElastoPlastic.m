d2938f036b6e4ebe1c41d020fb2d56a8db91b81b
SilvinWillemsen <silvinwillemsen@gmail.com>
2019-06-06 11:28:23 +0200

    Stable vector NR elastoplastic. Not getting expected bowing sounds yet

--- StiffStringElastoPlastic.m
+++ StiffStringElastoPlastic.m
@@ -2,58 +2,52 @@ clear all;
 close all;
 clc;
 
-drawString = false;
-Fs = 44100;     % Sampling rate
+%% Drawing functions
+drawString = false;     % Choose between two different drawing options (can only be true for elastoPlastic bow model)
 drawStart = 0;
+drawspeed = 100;
+
+%% Choose bow model (elastoPlastic, simple, hyperbolic, cos (raised cosine)
+bowModel = "elastoPlastic";
+
+Fs = 44100;     % Sampling rate
 f0 = 196.00;    % G3
 c = f0 * 2;     % Wave-Speed
-L = 1;
+L = 1;          % String length
 k = 1/Fs;       % Time-step
 s0 = 0.1;       % Damping coefficients
-s1 = 0.05;
+s1 = 0.005;
 
-E = 2e11;
-r = 0.0005; 
-A = r^2 * pi;
-Iner = pi*r^4 / 4;
-rho = 7850;
+E = 2e11;       % Young's modulus
+r = 0.0005;     % Radius
+A = r^2 * pi;   % Crossectional area
+Iner = pi*r^4 / 4; % Area moment of inertia
+rho = 7850;         % Density
 T = c^2 * rho * A;  % Tension
-kappa = sqrt(E*Iner/(rho*A));
-% kappa = 2;
-% B = 0.0001; %inharmonicity coefficient
-% kappa = sqrt(B)*(gamma/pi); % Stiffness Factor     
-% 
-% % Calculate grid spacing
-% h = sqrt((gamma^2*k^2 + 4 * s1 * k + sqrt((gamma^2 * k^2 + 4 * s1 * k)^2 + 16 * kappa^2 * k^2)) / 2);
-% N = floor(1/h); % Number of gridpoints
-% h = 1/N; % Recalculate gridspacing
-% 
-% % Courant numbers
-% lambdaSq = (gamma*k/h)^2; 
-% muSq = (k * kappa / h^2)^2;
+kappa = sqrt(E*Iner/(rho*A)); % Stiffness
 
-% [B, C, N, h, bB, bC] = newCreateString (c, kappa, L, s0, s1, k);
+%% Create string
 [B, C, N, h, Dxx, Dxxxx, s0, s1, bB, bC] = unscaledCreateStringNR(rho, A, T, E, Iner, L, s0, s1, k);
-% [B, C, N, h, bB, bC] = createStringElastoPlastic(c, kappa, L, s0, s1,
-% k);
-% [B, C, N, h, Dxx, Dxxxx] = newCreateString (c, kappa, L, s0, s1, k);
-% Initialise state vectors
+
+%% Initialise state vectors
 uPrev = zeros(N,1);
 u = zeros(N,1);
 uNext = zeros(N,1);  
 
-% Raised cosine
+%% Raised cosine
 width = 6;
 loc = 0.25;
 startIdx = floor(floor(loc * N) - width / 2);
 endIdx = floor(floor(loc * N) + width / 2);
-
-% u(startIdx : endIdx) = (1 - cos(2 * pi * [0:width] / width)) / 2;
+if bowModel == "cos"
+    u(startIdx : endIdx) = (1 - cos(2 * pi * [0:width] / width)) / 2;
+end
 uPrev = u;
 
 lengthSound = Fs * 5; % Set the length of the output
 out = zeros(lengthSound, 1);
 
+%% Initialise energy vectors
 kinEnergy = zeros(lengthSound, 1);
 potEnergy = zeros(lengthSound, 1);
 totEnergy = zeros(lengthSound, 1);
@@ -65,22 +59,7 @@ rOCdamp1StringEnergy = zeros(lengthSound, 1);
 rOCbowStringEnergy = zeros(lengthSound, 1);
 rOCenergy = zeros(lengthSound, 1);
 
-% Boundary condition (clamped, ss, free)
-bc = "clamped";
-
-%Bow Model
-a = 100;                % free parameter
-BM = sqrt(2*a)*exp(1/2);
-
-% User variables
-Vb = -0.2;               % Bowing speed
-Fb = 5;                % Bowing force / total mass of string
 pickup = floor(N/3);    % Pickup position
-
-% Initialise variables for Newton Raphson 
-tol = 1e-4;
-qSave = zeros (lengthSound, 1);
-qPrev = 0;
             
 bp = 1/4;
 bP = floor(bp * N);
@@ -88,75 +67,79 @@ I = zeros(N,1);
 I(floor(bp * N)) = 1;
 J = 1/h * I;
 
-%%%% the Contact Force (be with you) %%%%%%%%%
-mus=0.8;               % static friction coeff
-mud=0.3;              % dynamic friction coeff (must be < mus!!) %EDIT: and bigger than 0
-strv=1e-1;             % "stribeck" velocity
+if bowModel == "elastoPlastic"
+    %%%% the Contact Force (be with you) %%%%%%%%%
+    mus=0.8;               % static friction coeff
+    mud=0.3;              % dynamic friction coeff (must be < mus!!) %EDIT: and bigger than 0
+    strv=1e-1;             % "stribeck" velocity
 
-FnInit = 1;
-Fn = FnInit;
-fc=mud*Fn;             % coulomb force
-fs=mus*Fn;             % stiction force
-ssparam=[fc,fs,strv];            
-  
-sig0=1e4;           % bristle stiffness
-sig1=.1*sqrt(sig0);   % bristle damping
-sig2=0.4;            % viscous friction term
-sig3Init = 0;
-% sig0=4000;           % bristle stiffness
-% sig1=0;   % bristle damping
-% sig2=0.25;            % viscous friction term 
-sigma=[sig0,sig1,sig2];
+    FnInit = 1;
+    Fn = FnInit;
+    fc=mud*Fn;             % coulomb force
+    fs=mus*Fn;             % stiction force          
 
-z_ba=0.7*fc/sig0;    % break-away displacement (has to be < f_c/sigma_0!!) 
+    sig0 = 1e4;           % bristle stiffness
+    sig1 = 0.1*sqrt(sig0);   % bristle damping
+    sig2 = 0.4;            % viscous friction term
+    sig3 = 0;
+    
+    w = (2 * rand(lengthSound,1) - 1);
+    
+    sigma=[sig0,sig1,sig2];
 
-z1=0;               % initial mean bristle displacement
-y1=0;               % initial non-linear function
-f_fr1=0;                % initial friction force 
-f_tot_r1=0;         % initial total force on resonator
-f_tot_b1=0;         % initial total force on bow
+    z_ba=0.7*fc/sig0;    % break-away displacement (has to be < f_c/sigma_0!!) 
 
-VbInit = 0.1;
-a_z=[1,1/(2*Fs)];
-zPrev = 0;
-z = 0;
+    z1=0;               % initial mean bristle displacement
+    y1=0;               % initial non-linear function
+    f_fr1=0;                % initial friction force 
+    f_tot_r1=0;         % initial total force on resonator
+    f_tot_b1=0;         % initial total force on bow
 
-%%%%%% Simple Bow Model %%%%%%%
-a = 100;                % free parameter
-BM = sqrt(2*a)*exp(1/2);
-
-% Initialise variables for Newton Raphson 
-tol = 1e-7;
-qSave = zeros (lengthSound, 1);
-qPrev = 0;
-zdot = 0;
-zDotPrev = 0;
-bowModel = "simple";
-Vrel = VbInit;
-VrelPrev = VbInit;
-excitation = 0;
-% figure('Renderer', 'painters', 'Position', [100 100 1400 400])
-VbPrev = 0;
-bowVertPosPrev = 0;
-% K1 = -sig1 / ((sig2 + 2/k + 2*s0) * h);
-
-zVec = -1e-3:1e-5:1e-3;
-vRelVec = -0.5:0.001:0.5;
-for ii = 1 : length(vRelVec)
-    espon=exp(-(vRelVec(ii)/strv).^2);%exponential function
-    zssVec(ii) =sign(vRelVec(ii)).*(fc +(fs-fc)*espon)/sig0;   %steady state curve: z_ss(v)
-    if vRelVec(ii)==0
-      zssVec(ii)=fs/sig0;
+    Vb = 0.1;
+    zPrev = 0;
+    z = 0;
+    tol = 1e-7;
+    
+    zdot = Vb;
+    zDotPrev = 0;
+    
+    Vrel = Vb;
+    VrelPrev = Vb;
+    VbPrev = Vb;
+    
+    zVec = -1e-3:1e-5:1e-3;
+    vRelVec = -0.5:0.001:0.5;
+    for ii = 1 : length(vRelVec)
+        espon=exp(-(vRelVec(ii)/strv).^2);%exponential function
+        zssVec(ii) =sign(vRelVec(ii)).*(fc +(fs-fc)*espon)/sig0;   %steady state curve: z_ss(v)
+        if vRelVec(ii)==0
+          zssVec(ii)=fs/sig0;
+        end
     end
+    zSave = zeros(lengthSound, 1);
+    vRelSave = zeros(lengthSound, 1);
+    zDotSave = zeros(lengthSound, 1);
+    Fsave = zeros(lengthSound, 1);
+    iSave = zeros(lengthSound, 1);
+
+else
+    a = 100;                % free parameter
+    Vb = 0.1;               % Bowing speed
+    Fb = 1;                % Bowing force / total mass of string
+    BM = sqrt(2*a)*exp(1/2);
+    tol = 1e-4;
+    
+    qSave = zeros (lengthSound, 1);
+    qPrev = 0;
 end
-zSave = zeros(lengthSound, 1);
-vRelSave = zeros(lengthSound, 1);
-zDotSave = zeros(lengthSound, 1);
-Fsave = zeros(lengthSound, 1);
-iSave = zeros(lengthSound, 1);
+
+excitation = 0;
+
+bowVertPosPrev = 0; % for drawing
+
 scalar = 0;
-hysteresis = true;
-w = (2 * rand(lengthSound,1) - 1);
+hysteresis = false;
+
 % w = zeros(lengthSound,1);
 % wNoise = awgn(w,10);
 % plot(wNoise);
@@ -164,43 +147,27 @@ zssVecPlotVal = 1;
 % figure('Renderer', 'painters', 'Position', [100 100 900 500])
 eVec = 2:N-1;
 vec = 3:N-2;
-for t = 1 : lengthSound
-%     if round(scalar * 1000)/1000 == 1
-    if t < 5000
-        scalar = 1/5000 * t;
-    else
-        scalar = 1;
-    end
-% if t < drawStart
-    sig3(t) = sig3Init;
-% elseif t >= drawStart && t <= drawStart + Fs
-%     sig3(t) = sig3Init * (1 + cos(pi * (t - (drawStart)) / Fs)) * 0.5;
-% else
-%     sig3(t) = 0;
-% end
-    
-%     else
-%         scalar = sin(2 * pi * t / Fs);
-%     end
-    
-    Vb = VbInit * scalar;
-    Fn = abs(FnInit * 1);
-%     if Fn == 0
-%         Fn = 0.001;
-%     end
-    fc=mud*Fn;             % coulomb force
-    fs=mus*Fn;             % stiction force
-    z_ba=0.7*fc/sig0;    % break-away displacement (has to be < f_c/sigma_0!!) 
+scaleFact = rho * A * h;
 
-    bowVertPos = bowVertPosPrev + k/2 * (Vb + VbPrev);
-    VbPrev = Vb;
-    bowVertPosPrev = bowVertPos;
+for t = 1 : lengthSound
+
+    if bowModel == "elastoPlastic"
+        fc=mud*Fn;             % coulomb force
+        fs=mus*Fn;             % stiction force
+        z_ba=0.7*fc/sig0;    % break-away displacement (has to be < f_c/sigma_0!!) 
+    end
+    
+    % for drawing bow
+%     bowVertPos = bowVertPosPrev + k/2 * (Vb + VbPrev);
+%     VbPrev = Vb;
+%     bowVertPosPrev = bowVertPos;
+    
     if bowModel == "simple"
         b = 2/k * Vb + 2 * s0 * Vb + I' * bB * u + I' * bC * uPrev;
         eps = 1;
         i = 0;
         while eps>tol
-            q=qPrev-(1/h * Fb*BM*qPrev*exp(-a*qPrev^2)+2*qPrev/k+2*s0*qPrev+b)/...
+            q=qPrev-(1/h*Fb*BM*qPrev*exp(-a*qPrev^2)+2*qPrev/k+2*s0*qPrev+b)/...
              (1/h * Fb*BM*(1-2*a*qPrev^2)*exp(-a*qPrev^2)+2/k+2*s0);
             eps = abs(q-qPrev);
             qPrev = q;
@@ -225,8 +192,8 @@ for t = 1 : lengthSound
                 if Vrel==0
                   zss=fs/sig0;
                 end
-%                 zss = abs(zss);
-                %elasto-plastic function \alpha (v,z)
+
+                % elasto-plastic function \alpha (v,z)
                 zssPrev = zss;
                 zss = abs(zss);
                 alpha = 0;
@@ -240,51 +207,71 @@ for t = 1 : lengthSound
                     end
                 end
                 zss = zssPrev;
-                %non-linear function estimate
-                fnl=Vrel*(1-alpha*z/zss);
+
                 %% compute derivatives
                 
-                % dz_ss/dv
+                % Derivative of the steady state function (dz_ss/dv)
                 dz_ss = (-2*Vrel*sign(Vrel) / (strv^2*sig0))*(fs-fc)*espon;
                 
                 dalpha_v=0; %d(alpha)/dv 
                 dalpha_z=0; %d(alpha)/dz
                 
-                zss = abs(zss);
+                zss = abs(zss); 
                 if ((sign(z)==sign(Vrel)) && (abs(z)>z_ba) && (abs(z)<zss) )
-                    cosarg=cos(arg);
-                    dalpha_v=0.5*pi*cosarg*dz_ss*(z_ba-z)/(zss-z_ba)^2; 
-                    dalpha_z=0.5*pi*cosarg/(zss-z_ba);
+                    cosarg=cos(sign(z)*arg);
+                    dalpha_v=0.5*pi*cosarg*dz_ss*(z_ba-sign(z)*z)/(zss-z_ba)^2; 
+                    dalpha_z=0.5*pi*cosarg*sign(z)/(zss-z_ba);
                 end
                 zss = zssPrev;
+
+                % functions to perform newton raphson on
+                g1 = (2/k + 2 * s0) * Vrel + (sig0 * z + sig1 * zdot + sig2 * Vrel + sig3 * w(t)) / (rho * A * h) + b;
+                g2 = Vrel * (1 - alpha * z / zss) - zdot; %a^n (discrete zdot)
                 
-                d_fnlv = 1-z * ((alpha +Vrel*dalpha_v)*zss -dz_ss*alpha*Vrel)/zss^2;
-                d_fnlz = -Vrel/zss*(z*dalpha_z +alpha);
-%                 K1 = -k^2 / (h * (1 + s0 * k));
-                K1 = -sig1 / (sig2 + 2/k + 2*s0);
-                d_fnl = K1 * d_fnlv + d_fnlz * k / 2;
+                % derivatives of the functions
+                dg1v = 2/k + 2 * s0 + sig2 / (rho * A * h);
+                dg1z = sig0 / (rho * A * h) + 2 * sig1 / (k * rho * A * h);
+                dg2v = 1-z * ((alpha +Vrel*dalpha_v)*zss -dz_ss*alpha*Vrel)/zss^2;
+                dg2z = -Vrel/zss*(z*dalpha_z +alpha) - 2 / k;
                 
-                zdotNext = zdot - (fnl - zdot)/(d_fnl - 1);
+                % create Jacobian matrix
+                Jac = [dg1v, dg1z; dg2v, dg2z];
+                
+                % perform vector NR
+                solut = [Vrel; z] - Jac \ [g1; g2];
+
+                Vrel = solut(1);
+                z = solut(2);
+                
+                zdotNext = 2/k * (z - zPrev) - zDotPrev;
+%                 zdotNext = Vrel * (1-alpha * z / zss);
                 eps = abs(zdotNext-zdot);
                 zdot = zdotNext;
                 
-                z = zPrev + k / 2 * zDotPrev + k / 2 * zdot;
-                Vrel = (-sig0 * z - sig1 * zdot - sig3(t) * w(t) - b) / (sig2 + 2/k + 2*s0);
+                %what I used to do:
+%                 zdotNext = zdot - (fnl - zdot)/(d_fnl - 1);
+%                 z = zPrev + k / 2 * zDotPrev + k / 2 * zdot;
+%                 Vrel = ((-sig0 * z - sig1 * zdot - sig3(t) * w(t)) / scaleFact - b) / (sig2 / scaleFact + 2/k + 2*s0);
 %                 zTest(t,1) = (-sig1 * zdot - (sig2 + (rho * A) * (2/k + 2*s0)) * Vrel - sig3(t) * w(t) - (rho * A) * b) / sig0;
 %                 z - zTest(t)
                 i = i + 1;
                
             end
             iSave(t) = i;
-            F = (sig0 * z + sig1 * zdot + sig2 * Vrel + sig3(t) * w(t));
+            F = (sig0 * z + sig1 * zdot + sig2 * Vrel + sig3 * w(t));
             Fsave(t) = F;
         else 
             zdot=0; err=0; count=0;
             z=0; f_fr=0; v=vs;
         end
         excitation = J*F;
-%         excitation = 0;
         vRelSave(t) = Vrel;
+        zSave(t) = z;
+    
+        % update z and zdot
+        zPrev = z;
+        zDotPrev = zdot;
+    
     elseif bowModel == "hyperbolic"
         v = 1/k * (I' * u - I' * uPrev);
         mu = (mud + (mus - mud) * VbInit/2) / (VbInit/2 + v - Vb);
@@ -292,13 +279,11 @@ for t = 1 : lengthSound
     else
         excitation = 0;
     end
+    
+    %% Update FDS
     uNext = (B * u + C * uPrev) - excitation / (rho * A / k^2 + s0 / k);
-    zSave(t) = z;
     
-    zDotSave(t) = zdot;
-    zPrev = z;
-    zDotPrev = zdot;
-    
+    %% Calculate energy of the string
     kinEnergy(t) = rho * A / 2 * h * sum((1/k * (u - uPrev)).^2);
     potEnergy(t) = T / 2 * 1/h * sum((u(3:N) - u(2:N-1)) .* (uPrev(3:N) - uPrev(2:N-1)))...
         + E * Iner / 2 * 1/h^3 * sum((u(eVec+1) - 2 * u(eVec) + u(eVec-1)) ...
@@ -306,30 +291,22 @@ for t = 1 : lengthSound
     totEnergy(t) = kinEnergy(t) + potEnergy(t);
     
     
-    % String
+    %% Calculate Rate of Changes of Energy
     rOCkinEnergy(t) = h * rho * A / (2 * k^3) * sum((uNext - 2 * u + uPrev) .* (uNext - uPrev));
     rOCpotEnergy(t) = h * T / (2*k*h^2) * sum((u(vec+1) - 2 * u(vec) + u(vec-1)).* (uNext(vec) - uPrev(vec))) ...
          - h * E * Iner / (2 * k * h^4) * sum((u(vec+2) - 4 * u(vec+1) + 6 * u(vec) - 4 * u(vec-1) + u(vec-2)) .* (uNext(vec) - uPrev(vec)));%...
     rOCdamp0StringEnergy(t) = -2 * s0 * h / (4 * k^2) * sum((uNext - uPrev).*(uNext - uPrev));
     rOCdamp1StringEnergy(t) = 2 * h * s1 / (2 * k^2 * h^2) * sum((u(eVec+1) - 2 * u(eVec) + u(eVec-1) - uPrev(eVec+1) + 2 * uPrev(eVec) - uPrev(eVec-1)) .* (uNext(eVec) - uPrev(eVec)));
-    rOCbowStringEnergy(t) = -Fb * BM * q * exp(-a*q^2) .* (uNext(bP) - uPrev(bP)) / (2 * k);
+    rOCbowStringEnergy(t) = -h * sum(I .* excitation) * (uNext(bP) - uPrev(bP)) / (2 * k);
     rOCenergy(t) = rOCkinEnergy(t) - rOCpotEnergy(t) - rOCdamp0StringEnergy(t) - rOCdamp1StringEnergy(t) - rOCbowStringEnergy(t);
     
-    
-    
-    % Plo
-    if drawString == true && mod(t,10) == 0 && t > drawStart - 1000
+    %% Drawing functions
+    if drawString == true && mod(t,drawspeed) == 0 && t > drawStart - 1000
         clf
         subplot(2,1,1)
         plot(uNext);
         hold on;
-%         ylim([-1e-5 1e-5])
-        scatter(repmat(floor(bp*N), 20, 1), [-1e-5:1e-6:1e-5-1e-6]+mod(bowVertPos/20,1e-6),'.')
-%         if Vb < 0
-%             annotation("textarrow", [bp bp], [0.85 0.8]);
-%         else
-%             annotation("textarrow", [bp bp], [0.8 0.85]);
-%         end
+        scatter(repmat(floor(bp*N), 20, 1), [-1e-5:1e-6:1e-5-1e-6]+mod(bowVertPos/20,1e-6),'.');
         text(floor(bp*N) - 2, 0, "$V_B =$ " + num2str(Vb, 2), 'interpreter', 'latex', 'horizontalAlignment', 'right');
         title("String displacement at sample " + num2str(t), 'interpreter', 'latex','Fontsize', 16)
         if hysteresis == false
@@ -395,21 +372,12 @@ for t = 1 : lengthSound
             subplot(2,1,2)
             numDots = 500;   
             mat = [[1:-1/numDots:1/numDots]', [1:-1/numDots:1/numDots]', [1:-1/numDots:1/numDots]'];
-            vec = t-numDots + 1 : t;
+%             vec = t-numDots + 1 : t;
             cla;
             if t > numDots
 %                plot(vRelSave(t-numDots+1:t), Fsave(t-numDots+1:t), 'k', 'Linewidth', 2);
     %            hold on;
                scatter(vRelSave(t-numDots+1:t), Fsave(t-numDots+1:t), 40, mat);
-
-
-    %             for i = 1:length(vec)-1
-    %                 
-    %                 line(vRelSave(vec(i):vec(i+1)),...
-    %                      Fsave(vec(i):vec(i+1)),...
-    %                      'color', mat(i,:));
-    %             end
-    %         xlim ([-0.5 0.5]);
             end
             ylabel("$f(v,z)$", 'interpreter', 'latex')
             xlabel("$v$", 'interpreter', 'latex')
@@ -421,14 +389,17 @@ for t = 1 : lengthSound
             grid on;
         end
         drawnow;
-    else
+    elseif mod(t, drawspeed) == 0
         subplot(3,1,1);
         plot(u);
+        title("String (state)")
         subplot(3,1,2);
         plot(totEnergy(10:t) / totEnergy(10) - 1);
+        title("Total energy")
         subplot(3,1,3);
         cla 
         plot(rOCenergy(10:t));
+        title("Rate of change of energy + bowing energy (should be 0)");
 %         hold on; 
 %         plot(rOCbowStringEnergy(10:t))
         drawnow;
